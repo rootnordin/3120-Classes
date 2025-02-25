518b6fadd3fed9da76924718075161564d360c67
Feruza29 <feruza29@me.com>
2025-02-24 19:12:31 -0500

    Update Animal.py

--- Animal.py
+++ Animal.py
@@ -6,3 +6,11 @@ class Animal:
     def talk(self):
         print("hi")
 
+def set_name9(self, new_name):
+    " Change the name of the animal"
+    self.__name == new_name
+    print("My new name is", self.__name)
+
+def get_name(self):
+    " Return the name of the animal"
+    return self.__name
