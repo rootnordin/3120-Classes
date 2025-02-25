5e240c36b7a2059d030d193155a75b63dce6dfed
Feruza29 <feruza29@me.com>
2025-02-24 19:49:39 -0500

    lol

--- Animal.py
+++ Animal.py
@@ -6,11 +6,43 @@ class Animal:
     def talk(self):
         print("hi")
 
-def set_name9(self, new_name):
-    " Change the name of the animal"
-    self.__name == new_name
-    print("My new name is", self.__name)
+class Animal:
+    def __init__(self, name, species):
+        self.__name = name
+        self.__species = species
+        print(f"Hello, I am {self.__name}, a {self.__species}")
 
-def get_name(self):
-    " Return the name of the animal"
-    return self.__name
+    def talk(self):
+        print("Hi")
+
+    def eat(self, food):
+        print(f"{self.__name} is eating {food}.")
+
+    def sleep(self, hours):
+        print(f"{self.__name} is sleeping for {hours} hours.")
+
+    def walk(self, distance):
+        print(f"{self.__name} walked {distance} kilometers.")
+
+    def describe(self):
+        print(f"My name is {self.__name} and I am a {self.__species}.")
+
+    def set_name(self, new_name):
+        self.__name = new_name
+        print(f"My new name is {self.__name}.")
+
+    def make_sound(self, sound):
+        print(f"{self.__name} says {sound}!")
+
+    def play(self, activity):
+        print(f"{self.__name} is playing {activity}.")
+
+    def age(self, years):
+        print(f"{self.__name} is {years} years old.")
+
+    def run(self, speed):
+        print(f"{self.__name} is running at {speed} km/h.")
+
+    def jump(self, height):
+        print(f"{self.__name} jumped {height} meters high.")
+    
